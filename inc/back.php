e69fe54d0db7162080acb8278b2e093fd381afa9
cuunonhaitac <cuunonhaitac@gmail.com>
2016-05-26 16:01:30 +0700

    Detail Page Board Game
    - Fix error search
    - Implement detail page

---
+++ inc/back.php
@@ -0,0 +1,11 @@
+<div class="empty-space"></div>
+<div data-role="footer" data-position="fixed" data-fullscreen="true" id="button-back">
+	<button class="sgbg-back" data-rel="back" data-transition="slide" data-direction="reverse">BACK</button>
+</div>
+<script>
+$(function(){
+	$(".sgbg-back").click(function(){
+		window.history.back();
+	});
+})
+</script>
--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 <div class="empty-space"></div>
 <div data-role="footer" data-position="fixed" data-fullscreen="true">
-	<form action="../inc/search.php" method="post" id="search-form">
-		<input type="search" name="search" id="search-basic" value="" placeholder="Which game will it be today?" />
+	<form action="../inc/search.php" method="get" id="search-form">
+		<input type="search" name="id" id="search-basic" value="" placeholder="Which game will it be today?" />
 	</form>	
 </div>
